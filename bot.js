86729bdd6fc543e14297ee59204cf74fb2e4b6be
root <root@localhost.localdomain>
2018-10-26 00:43:21 +0100

    second commit

---
+++ bot.js
@@ -0,0 +1,125 @@
+console.log('Twitter Bot Is Starting');
+
+var tweet_content;
+
+var Twit = require('twit');
+
+const mastodon = require('mastodon-api'); 
+
+var config = require('./config');
+var T = new Twit(config);
+
+var M = new mastodon({
+	client_key: ' 73665993e6020bcc030e04ab19aaf787390f8db9d2d5712d90a19ac869abb6b3 ',
+	client_secret: 'e729a5aebda4ba7f1d2255dc99b1ecfa4aca58bf84294c14310b30f2efcf7881',
+	access_token: 'fdc0cc0ebc89f37c04cbc471fd59799236c0d664f1b509daa6ae716875ddd191',
+	timeout_ms: 60*1000,
+	api_url: 'https://botsin.space/api/v1/',
+})
+
+
+var stream = T.stream('statuses/filter', { follow: [2282671736, 1244325504, 3241065977,  	
+
+828640738065805312, 2820666877]})
+
+stream.on('tweet', tweetEvent); 
+
+function tweetEvent(tweet){
+	var fs = require('fs');
+	var json = JSON.stringify(tweet, null, 2);
+	fs.writeFile("tweet.json", json);
+	console.log(tweet)
+	
+	if(tweet.retweeted_status){
+		console.log('Retweet')
+	}
+	else if(tweet.truncated == false){
+		console.log(tweet.truncated)
+		console.log('Extended')
+		tweet_content = 'From ' + tweet.user.name + ':' + tweet.text
+		MastaPost()
+	}
+	else if(tweet.truncated == true){
+		console.log(tweet.truncated);
+		console.log(tweet.extended_tweet.full_text)
+		console.log('Normal')
+		tweet_content = 'From ' + tweet.user.name + ':' + tweet.extended_tweet.full_text + " #Arrowverse #TheCW"
+		MastaPost();
+		}
+}
+
+
+function GetTweet() {
+		
+	var twitterparams = {
+		q: '-RT from:CW_TheFlash',
+		tweet_mode: 'extended',
+		count: 1
+		}
+
+	T.get('search/tweets', twitterparams, gotData);
+
+	function gotData(err, data, response) {
+		//console.log(data)
+		var tweets = data.statuses;
+		for(var i = 0; i < tweets.length; i++){
+			if(tweets[i].retweeted_status){
+				console.log('The Flash Reweeted: ' + tweets[i].full_text);
+			}
+			else{
+				console.log(tweets[i].full_text);
+				tweet_content = tweets[i].full_text;
+			}
+		}
+		if(err){
+			console.log("Something went wrong with gotData!");
+		}
+		else{
+			console.log("It Worked!");
+		}
+	}
+}
+
+function TweetIt() {
+
+	//var r = Math.floor(Math.random()*100);
+		var tweet = {
+		status: tweet_content
+	}
+
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, response) {
+		if(err){
+			console.log("Something went wrong when tweeting");
+		}
+		else{
+			console.log("I Tweeted!");
+		}
+	}
+}
+
+
+function MastaPost() {
+
+	var mastaparams ={
+		status: tweet_content
+	}
+	
+	console.log(tweet_content);
+	M.post('statuses', mastaparams, (error, mdata) => {
+	if(error) {
+		console.error(error);
+	}
+	else{
+		console.log(tweet_content)
+		console.log(mdata)
+		console.log('Toot!')
+	}
+})
+}
+
+
+//setInterval(GetTweet, 1000*5)
+//setInterval(MastaPost, 1000*6)
+
---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+
+  consumer_key:         'kP81sGCSN62kwQBLnbtKkul8o',
+  consumer_secret:      'FqaTnp7jVwJe0QvKRyrZLIncxpMMzwzJDwqxKnKOPslzo6vCDK',
+  access_token:         '954091939485442049-Lh9M9COrTbSC4IPvkgk90eKhZs2wQ0o',
+  access_token_secret:  'i4t9GyQjyt3qPd0Owl3tOde1c1wZq6WYwGCRC0A8ZUX0T',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
